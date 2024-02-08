6ccf1a5a851880b3618e78c7b0f5bdd5de223338
René Ladan <rene0+github@freedom.nl>
2024-02-08 22:21:29 +0100

    examples/set_date_time.rs: show that day-of-week must be set before day-of-month.

--- examples/set_date_time.rs
+++ examples/set_date_time.rs
@@ -4,13 +4,16 @@ fn main() {
 
     // set date of now, leave out extra checks, do not compare to previous non-existing value:
 
+    // day must be set *after* year, month, and weekday because set_day() checks if its argument
+    // is within the current month which could be 28 or 29 for February depending on the year and
+    // day-of-week for 00 years.
+
     dcf77.set_year(Some(24), true, false);
     // year is clipped to century
     dcf77.set_month(Some(1), true, false);
+    dcf77.set_day(Some(25), true, false);
+    println!("Day-of-month: {:?}", dcf77.get_day());
     dcf77.set_weekday(Some(4), true, false);
-    // day must be set *after* year, month, and weekday because set_day() checks if its argument
-    // is within the current month which could be 28 or 29 for February depending on the year and
-    // day-of-week for 00 years:
     dcf77.set_day(Some(25), true, false);
     dcf77.set_hour(Some(22), true, false);
     dcf77.set_minute(Some(34), true, false);
